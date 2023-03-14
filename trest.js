42417391298cd65c84af084da3e00943dd95deb6
rhsabbir1 <rhsabbir090@gmai.com>
2023-03-14 21:17:54 +0600

    show Phone Images

--- trest.js
+++ trest.js
@@ -3,7 +3,7 @@
 // 2. after typing something on the search textbox, you should be able to press enter to get search result -----(done)
 // 3. spinner is always running. should be displayed at the time of data loading-----(Done)
 // 4. If you search for 'iphone', check the api, you should get a lot of data but the UI displays only one phone----(done)
-// 5. Search is not clearing previous results
+// 5. Search is not clearing previous results---(done)
 // 6. picture of the phones are not showing
 // 7. Show All button should be hidden after all data is loaded
 // 8. Load phone details doesn't display anything
