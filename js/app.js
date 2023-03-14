bb44367489a5a9efeac240a54621840bc6765cc4
rhsabbir1 <rhsabbir090@gmai.com>
2023-03-14 22:19:05 +0600

    text show in mpdal

--- js/app.js
+++ js/app.js
@@ -32,6 +32,7 @@ const displayPhones = (phones, dataLimit) => {
     }
     // display all phones
     phones.forEach(phone => {
+     
         const phoneDiv = document.createElement('div');
         phoneDiv.classList.add('col');
         phoneDiv.innerHTML = `
@@ -89,23 +90,24 @@ document.getElementById('btn-show-all').addEventListener('click', function () {
 })
 
 const loadPhoneDetails = async id => {
-    const url = `www.openapi.programming-hero.com/api/phone/${id}`;
+    
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
     const res = await fetch(url);
     const data = await res.json();
-    displayPhoneDetails(data.data);
+    displayPhoneDetails(data);
 }
 
 const displayPhoneDetails = phone => {
-    console.log(phone);
+    console.log(phone)
     const modalTitle = document.getElementById('phoneDetailModalLabel');
-    modalTitle.innerText = phone.name;
+    modalTitle.innerText = phone.data.name;
     const phoneDetails = document.getElementById('phone-details');
-    console.log(phone.mainFeatures.sensors[0]);
+    // console.log(phone.mainFeatures.sensors[0]);
     phoneDetails.innerHTML = `
-        <p>Release Date: ${phone.releaseDate}</p>
+        <p>Release Date: ${phone.data.releaseDate}</p>
         <p>Storage: ${phone.mainFeatures}</p>
         <p>Others: ${phone.others ? phone.others.Bluetooth : 'No Bluetooth Information'}</p>
-        <p>Sensor: ${phone.mainFeatures.sensors ? phone.mainFeatures.sensors[0] : 'no sensor'}</p>
+        <p>Sensor: ${phone.mainFeatures?.sensors ? phone.mainFeatures.sensors[0] : 'no sensor'}</p>
     `
 }
 
